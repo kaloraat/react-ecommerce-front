ade410ef8ac590db0047cbc263e45fd760cffaa8
Ryan Dhungel <kaloraat@email.com>
2020-10-04 13:05:00 +1100

    updated login page and package.json

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -8,8 +8,8 @@ import { Link } from "react-router-dom";
 import { createOrUpdateUser } from "../../functions/auth";
 
 const Login = ({ history }) => {
-  const [email, setEmail] = useState("gqlreactnode@gmail.com");
-  const [password, setPassword] = useState("gggggg");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
   const { user } = useSelector((state) => ({ ...state }));
